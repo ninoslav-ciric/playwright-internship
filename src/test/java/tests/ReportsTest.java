d526e84b2e79d101d1bbd8156d3929f53fb05cf2
vanja <vanja.stankovic@htecgroup.com>
2025-10-30 16:23:08 +0100

    Allure

--- src/test/java/tests/ReportsTest.java
+++ src/test/java/tests/ReportsTest.java
@@ -4,9 +4,11 @@ import base.TestBase;
 import assertion.LoginPageAsserts;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 import com.microsoft.playwright.Locator;
 import org.testng.asserts.SoftAssert;
+import utils.AllureClass;
 
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
@@ -21,43 +23,56 @@ import static utils.ConfigReader.getValidPassword;
 public class ReportsTest extends TestBase {
     private final SoftAssert softAssert = new SoftAssert();
 
-    @Test
+    @Test(groups = {"positive"},
+            description = "Verify successful creating of new report"
+    )
+    @Description("Verify successful creating of new report")
     private void testReportsValid() throws AWTException {
+        AllureClass.logStep("Login to page.");
         loginPage.loginExpectSuccess(getValidEmail(), getValidPassword());
 
         LoginPageAsserts loginPageAsserts = new LoginPageAsserts();
         loginPageAsserts.validateLogin(loginPage);
 
+        AllureClass.logStep("Navigate to reports.");
         page.locator("//*[@id=\"root\"]/div/div[3]/div[1]/div/div/a[5]").click();
         page.locator("//*[@id=\"root\"]/div/div[3]/div[2]/div/div[1]/div/div/div/span/a").click();
 
         //Summary
+        AllureClass.logStep("Fill input for summary.");
         Locator summaryInput = page.getByPlaceholder("Summary");
         summaryInput.fill("Vanja test 123");
 
+        AllureClass.logStep("Select option Task.");
         Locator dropdownTask = page.locator("select[name='type']");
         dropdownTask.selectOption("Task");
 
+        AllureClass.logStep("Select option Medium.");
         Locator dropdownSeverity = page.locator("select[name='severity']");
         dropdownSeverity.selectOption("Medium");
 
+        AllureClass.logStep("Select option Minor.");
         Locator dropdownPriority = page.locator("select[name='priority']");
         dropdownPriority.selectOption("Minor");
 
+        AllureClass.logStep("Fill input for description.");
         Locator descriptionInput = page.getByPlaceholder("Description");
         descriptionInput.click();
         descriptionInput.fill("Opis Vanja jedan dva tri");
 
+        AllureClass.logStep("Fill input for reproduction step.");
         Locator reproductionStepInput = page.getByPlaceholder("Reproduction step");
         reproductionStepInput.click();
         reproductionStepInput.fill("Korak jedan");
 
+        AllureClass.logStep("Fill input for expected result.");
         Locator resultInput = page.getByPlaceholder("Expected result");
         resultInput.click();
         resultInput.fill("Rezultat jedan dva tri");
 
         //page.locator("input[name='image']").setInputFiles(Paths.get("C:\\Users\\VanjaStankovic\\Desktop\\Vanja Stankovic Playwright\\src\\test\\screenshot\\Screenshot 2025-10-24 105836.png"));
         //page.locator("input[name='image']").click();
+        AllureClass.logStep("Click button to add image.");
         page.locator("//*[@id=\"root\"]/div/div[3]/div[2]/div/div/div[2]/div[8]/div/div[2]/div[2]/label").click();
 
         Robot robot = new Robot();
@@ -78,6 +93,7 @@ public class ReportsTest extends TestBase {
 
         robot.delay(1000);
 
+        AllureClass.logStep("Click button submit.");
         Locator submitButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Submit"));
         submitButton.click();
 
@@ -88,16 +104,22 @@ public class ReportsTest extends TestBase {
         softAssert.assertAll();
     }
 
-    @Test
+    @Test(dependsOnMethods = {"testReportsValid"}, groups = {"negative"},
+            description = "Verify invalid creating of new report"
+    )
+    @Description("Verify invalid creating of new report")
     private void reportTestCaseInvalid(){
+        AllureClass.logStep("Login to page.");
         loginPage.loginExpectSuccess(getValidEmail(), getValidPassword());
 
         LoginPageAsserts loginPageAsserts = new LoginPageAsserts();
         loginPageAsserts.validateLogin(loginPage);
 
+        AllureClass.logStep("Navigate to reports.");
         page.locator("//*[@id=\"root\"]/div/div[3]/div[1]/div/div/a[5]").click();
         page.locator("//*[@id=\"root\"]/div/div[3]/div[2]/div/div[1]/div/div/div/span/a").click();
 
+        AllureClass.logStep("Click button submit.");
         Locator submitButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Submit"));
         submitButton.click();
 
@@ -118,15 +140,21 @@ public class ReportsTest extends TestBase {
 
     }
 
-    @Test
+    @Test(dependsOnMethods = {"testReportsValid", "reportTestCaseInvalid"}, groups = {"positive"},
+            description = "Verify data in preview section"
+    )
+    @Description("Verify data in preview section")
     private void previewReportTest(){
+        AllureClass.logStep("Login to page.");
         loginPage.loginExpectSuccess(getValidEmail(), getValidPassword());
 
         LoginPageAsserts loginPageAsserts = new LoginPageAsserts();
         loginPageAsserts.validateLogin(loginPage);
 
+        AllureClass.logStep("Navigate to reports.");
         page.locator("//*[@id=\"root\"]/div/div[3]/div[1]/div/div/a[5]").click();
 
+        AllureClass.logStep("Navigate to preview of report.");
         Locator divElement = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Vanja test 123 Issue type:"));
         divElement.getByText("Preview").click();
 
@@ -139,21 +167,61 @@ public class ReportsTest extends TestBase {
         softAssert.assertAll();
     }
 
-    @Test
+    @Test(dependsOnMethods = {"testReportsValid", "reportTestCaseInvalid", "previewReportTest"}, groups = {"positive"},
+            description = "Verify successful deleting of report"
+    )
+    @Description("Verify successful deleting of report")
     private void deleteReportTest(){
+        AllureClass.logStep("Login to page.");
         loginPage.loginExpectSuccess(getValidEmail(), getValidPassword());
 
         LoginPageAsserts loginPageAsserts = new LoginPageAsserts();
         loginPageAsserts.validateLogin(loginPage);
 
+        AllureClass.logStep("Navigate to reports.");
         page.locator("//*[@id=\"root\"]/div/div[3]/div[1]/div/div/a[5]").click();
 
+        AllureClass.logStep("Navigate to specific report.");
         Locator divElement = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Vanja test 123 Issue type:"));
         divElement.click();
 
+        AllureClass.logStep("Click button Remove");
         Locator deleteButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Remove"));
         deleteButton.click();
 
+        AllureClass.logStep("Click button trash");
+        Locator removeElement = page.locator("#react-confirm-alert").getByText("Remove", new Locator.GetByTextOptions().setExact(true));
+        removeElement.click();
+
+        Locator removedElement = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Vanja test 123 Issue type: Task"));
+
+        softAssert.assertFalse(removedElement.isVisible(), "Report should be deleted");
+        softAssert.assertAll();
+    }
+
+    @Test(dependsOnMethods = {"testReportsValid", "reportTestCaseInvalid", "previewReportTest", "deleteReportTest"}, groups = {"positive"},
+            description = "Verify successful deleting of report"
+    )
+    @Description("Verify successful deleting of report")
+    private void deleteReportTestFail(){
+        AllureClass.logStep("Login to page.");
+        loginPage.loginExpectSuccess(getValidEmail(), getValidPassword());
+
+        LoginPageAsserts loginPageAsserts = new LoginPageAsserts();
+        loginPageAsserts.validateLogin(loginPage);
+
+        AllureClass.logStep("Navigate to reports.");
+        page.locator("//*[@id=\"root\"]/div/div[3]/div[1]/div/div/a[5]").click();
+
+        AllureClass.logStep("Navigate to specific report.");
+        Locator divElement = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Nesto 123:"));
+        divElement.click();
+
+        AllureClass.logStep("Click button Remove");
+        Locator deleteButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Remove"));
+        deleteButton.click();
+
+        AllureClass.logStep("Click button trash");
         Locator removeElement = page.locator("#react-confirm-alert").getByText("Remove", new Locator.GetByTextOptions().setExact(true));
         removeElement.click();
 
--- src/test/java/tests/TestCasesTest.java
+++ src/test/java/tests/TestCasesTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import assertion.LoginPageAsserts;
 import base.TestBase;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
@@ -9,6 +11,9 @@ import com.microsoft.playwright.TimeoutError;
 import com.microsoft.playwright.options.AriaRole;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
+import utils.AllureClass;
+
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
 
 import static utils.ConfigReader.getValidEmail;
 import static utils.ConfigReader.getValidPassword;
@@ -16,35 +21,44 @@ import static utils.ConfigReader.getValidPassword;
 public class TestCasesTest extends TestBase {
     private final String TESTCASE_BUTTON_SELECTOR = "a[href='/testcase']";
     private final SoftAssert softAssert = new SoftAssert();
-    @Test
+    @Test(groups = {"positive"},
+            description = "Verify successful creating of new test case"
+    )
+    @Description("Verify successful creating of new test case")
     public void testTestCaseValid(){
-
+        AllureClass.logStep("Login to page.");
         loginPage.loginExpectSuccess(getValidEmail(), getValidPassword());
 
         LoginPageAsserts loginPageAsserts = new LoginPageAsserts();
         loginPageAsserts.validateLogin(loginPage);
 
+        AllureClass.logStep("Navigate to test case.");
         page.locator("//*[@id=\"root\"]/div/div[3]/div[1]/div/div/a[3]").click();
         page.locator("//*[@id=\"root\"]/div/div[3]/div[2]/div/div[1]/div/div/div/span/a").click();
 
         //Title
+        AllureClass.logStep("Fill input for title.");
         Locator titleInput = page.getByPlaceholder("Title");
         titleInput.fill("Vanja test");
 
         //Description
+        AllureClass.logStep("Fill input for description.");
         Locator descriptionInput = page.getByPlaceholder("Description");
         descriptionInput.fill("Opis problema jedan dva tri");
 
         //Expected Result
+        AllureClass.logStep("Fill input for expected result.");
         Locator expectedResultInput = page.getByPlaceholder("Expected Result");
         expectedResultInput.fill("Nesto se desava ovde");
 
         //Test step
+        AllureClass.logStep("Fill input for test step.");
         Locator testStepInput = page.getByPlaceholder("Test step");
         testStepInput.fill("Korak broj jedan");
 
 
         //Button Submit
+        AllureClass.logStep("Click submit button.");
         Locator submitButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Submit"));
         submitButton.click();
 
@@ -57,16 +71,22 @@ public class TestCasesTest extends TestBase {
         softAssert.assertAll();
     }
 
-    @Test
+    @Test(dependsOnMethods = {"testTestCaseValid"}, groups = {"negative"},
+            description = "Verify invalid creating of new test case"
+    )
+    @Description("Verify invalid creating of new test case")
     private void testTestCaseInvalid(){
+        AllureClass.logStep("Login to page.");
         loginPage.loginExpectSuccess(getValidEmail(), getValidPassword());
 
         LoginPageAsserts loginPageAsserts = new LoginPageAsserts();
         loginPageAsserts.validateLogin(loginPage);
 
+        AllureClass.logStep("Navigate to test case.");
         page.locator("//*[@id=\"root\"]/div/div[3]/div[1]/div/div/a[3]").click();
         page.locator("//*[@id=\"root\"]/div/div[3]/div[2]/div/div[1]/div/div/div/span/a").click();
 
+        AllureClass.logStep("Click submit button.");
         Locator submitButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Submit"));
         submitButton.click();
 
@@ -78,21 +98,27 @@ public class TestCasesTest extends TestBase {
 
     }
 
-    @Test
+    @Test(dependsOnMethods = {"testTestCaseValid", "testTestCaseInvalid"}, groups = {"positive"},
+            description = "Verify successful deleting of test case"
+    )
+    @Description("Verify successful deleting of test case")
     private void deleteTestCaseTest(){
+        AllureClass.logStep("Login to page.");
         loginPage.loginExpectSuccess(getValidEmail(), getValidPassword());
 
         LoginPageAsserts loginPageAsserts = new LoginPageAsserts();
         loginPageAsserts.validateLogin(loginPage);
-
+        AllureClass.logStep("Navigate to test case.");
         page.locator("//*[@id=\"root\"]/div/div[3]/div[1]/div/div/a[3]").click();
 
+        AllureClass.logStep("Navigate to specific test case that needs to be deleted.");
         Locator divElement = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Vanja test Description: Opis"));
         divElement.click();
 
+        AllureClass.logStep("Click the delete button");
         Locator deleteButton = page.locator("button:has(i.far.fa-trash-alt)").first();
         deleteButton.click();
-
+        AllureClass.logStep("Click the remove button");
         Locator removeElement = page.locator("#react-confirm-alert").getByText("Remove", new Locator.GetByTextOptions().setExact(true));
         removeElement.click();
 
