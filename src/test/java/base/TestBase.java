d67d3f2877f68a0179b1e16161408f28fdbe707a
Anita Aleksic <anita.aleksic@htecgroup.com>
2025-10-31 15:28:23 +0100

    allure

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,6 +1,8 @@
 package base;
 
 import com.microsoft.playwright.*;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import pages.DashboardPage;
@@ -8,6 +10,9 @@ import pages.LoginPage;
 import pages.PlaygroundPage;
 import pages.TestCasePage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Paths;
 
 import static utils.ConfigReader.*;
@@ -21,19 +26,19 @@ public class TestBase {
     protected DashboardPage dashboardPage;
     protected PlaygroundPage playgroundPage;
 
-    @BeforeSuite
+    @BeforeSuite(alwaysRun = true)
     public static void setupClass() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public static void tearDownClass() {
         browser.close();
         playwright.close();
     }
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void setup() {
         page = browser.newPage();
         page.context().tracing().start(new Tracing.StartOptions()
@@ -49,29 +54,45 @@ public class TestBase {
 
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown(org.testng.ITestResult result) {
         if (result.getStatus() == org.testng.ITestResult.FAILURE) {
             String tracePath = "target/playwright-report/trace-" + System.currentTimeMillis() + ".zip";
             String screenShotPath = "screenshots/" + System.currentTimeMillis() + ".png";
             page.context().tracing().stop(new Tracing.StopOptions().setPath(Paths.get(tracePath)));
             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(screenShotPath)));
+            try (FileInputStream fis = new FileInputStream(screenShotPath)) {
+                Allure.addAttachment("Screenshot", fis);
+            } catch (IOException e) {
+                System.err.println("Failed to attach screenshot: " + e.getMessage());
+            }
+
+
         } else {
             page.context().tracing().stop();
         }
         page.close();
     }
 
+    @Step("Log in to the application")
     public void login(){
         loginPage.loginExpectSuccess(getValidEmail(), getValidPassword());
         Assert.assertTrue(loginPage.isDashboardVisible(), "Dashboard should be visible");
     }
 
+    @Step("Invalid log in to cause dependent test to be skipped")
+    public void InvalidLogin(){
+        loginPage.loginExpectSuccess("invalid", "invalid");
+        Assert.assertTrue(loginPage.isDashboardVisible(), "Dashboard should be visible");
+    }
+
+    @Step("Navigate to Test case page")
     public void navigateToTestCasePage(){
         dashboardPage.getMenuItemByName("Test Cases").click();
         Assert.assertTrue(testCasePage.isTestcasePageVisible(), "Test case page should be visible");
     }
 
+    @Step("Navigate to Playground page")
     public void navigateToPlaygroundPage(){
         dashboardPage.getMenuItemByName("Playground").click();
         Assert.assertTrue(playgroundPage.isPlaygroundPageVisible(), "Playground page should be visible");
--- src/test/java/tests/playground/DeleteProject.java
+++ src/test/java/tests/playground/DeleteProject.java
@@ -4,20 +4,29 @@ import assertion.PlaygroundPageAsserts;
 import assertion.TestCasePageAsserts;
 import base.TestBase;
 import org.testng.annotations.Test;
+import utils.AllureSteps;
 
 public class DeleteProject extends TestBase {
+    AllureSteps steps = new AllureSteps();
     //positive
-    @Test(groups = {"smoke", "UI"},
+    @Test(groups = {"positive"},
             description = "Verify successful deletion of a project"
     )
     public void testDeleteTestCase(){
+        steps.logStep("Login to the application");
         login();
+
+        steps.logStep("Navigate to Playground page");
         navigateToPlaygroundPage();
 
         String title = playgroundPage.getRandomProject().textContent();
+
+        steps.logStep("Deleting project with title: " + title);
         playgroundPage.deleteProjectExpectSuccess(title);
 
         PlaygroundPageAsserts playgroundPageAsserts = new PlaygroundPageAsserts();
+
+        steps.logStep("Verify the deleted project is not visible");
         playgroundPageAsserts.assertDeleteProject(playgroundPage, title);
     }
 }
